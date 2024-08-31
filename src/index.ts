02afe33f461e0f263335db21a43d5bc1d5d46448
Garima Mittal <mercurialVeracious@gmail.com>
2024-08-31 10:17:46 +0530

    Refactor Track commands

--- src/index.ts
+++ src/index.ts
@@ -22,31 +22,23 @@ readInput.on('line', (line) => {
       case Command.PLACE:
         const robotLocation = text[1];
         robot.place(robotLocation);
-        robot.trackCommand(Command.PLACE);
         break;
       case Command.LEFT:
         robot.left();
-        robot.trackCommand(Command.LEFT);
         break;
       case Command.RIGHT:
         robot.right();
-        robot.trackCommand(Command.RIGHT);
         break;
       case Command.MOVE:
         robot.move();
-        robot.trackCommand(Command.MOVE);
         break;
       case Command.REPORT:
         robot.report();
-        robot.trackCommand(Command.REPORT);
         break;
       case Command.TRACK:
         const count = parseInt(text[1]);
         if(count>0){
-         const commands =  robot.commands.slice(0,count);
-         commands.forEach(command=>{
-          console.log("(", command.coordiante.x, " ", command.coordiante.y, " ", command.direction, ") ", command.command);
-         })
+          robot.printLastNCommands(count);
         }
         else{
           console.log("Provide a valid number for tracking");
--- src/models/robot.ts
+++ src/models/robot.ts
@@ -5,24 +5,27 @@ export type Coordinate = {
     x: number,
     y: number
 }
-
+type Location ={
+    coordinate: Coordinate, direction: Direction
+}
+type TrackCommand = {currentLocation:Location, prevLocation: Location, command: Command};
 export class Robot{
-    coordinate: Coordinate;
-    direction: Direction;
+    latestLocation: Location
     coordinateLimit: Coordinate
-    commands: {coordiante: Coordinate, direction: Direction, command: Command}[]
+    commands: TrackCommand[]
 
-    constructor(coordinateLimit: Coordinate, coordinate={x:0,y:0}, direction = Direction.NORTH, commands =[]){
-       this.coordinate = coordinate;
-       this.direction = direction;
+    constructor(coordinateLimit: Coordinate, latestLocation = {coordinate:{x:0,y:0}, direction: Direction.NORTH}, commands =[]){
+       this.latestLocation = latestLocation;
        this.coordinateLimit = coordinateLimit;
        this.commands = commands;
     }
      move = () => {
-        const location = { x:DirectionMap[this.direction].MOVE.x  + this.coordinate.x, 
-            y: DirectionMap[this.direction].MOVE.y  + this.coordinate.y }
+        const prevLocation ={ ...this.latestLocation };
+        const location = { x:DirectionMap[this.latestLocation.direction].MOVE.x  + this.latestLocation.coordinate.x, 
+            y: DirectionMap[this.latestLocation.direction].MOVE.y  + this.latestLocation.coordinate.y }
         if(this.validateLocation(location)){
-            this.coordinate = location;
+            this.latestLocation.coordinate = location;
+            this.trackCommand(Command.MOVE, prevLocation);
         }
         else{
             console.log("Will fall off from edge, skipping the command MOVE");
@@ -30,27 +33,47 @@ export class Robot{
     }
     
      left = () => {
-        this.direction = DirectionMap[this.direction].LEFT;
-        
+        const prevLocation ={ ...this.latestLocation };
+        this.latestLocation.direction = DirectionMap[this.latestLocation.direction].LEFT;
+        this.trackCommand(Command.LEFT, prevLocation);
     }
      right = () => {
-        this.direction = DirectionMap[this.direction].RIGHT;
+        const prevLocation ={ ...this.latestLocation };
+        this.latestLocation.direction = DirectionMap[this.latestLocation.direction].RIGHT;
+        this.trackCommand(Command.RIGHT, prevLocation);
     }
     
      report = () =>{
-        console.log(" Robot is currently at ", this.coordinate, "facing ", this.direction);
+        console.log(" Robot is currently at ", this.latestLocation.coordinate, "facing ", this.latestLocation.direction);
     }
      place = (robotLocation:string) =>{
+        const prevLocation ={ ...this.latestLocation };
         const robotInstructions = robotLocation.split(",");
         const coordinate: Coordinate ={ x:parseInt(robotInstructions[0]),y: parseInt(robotInstructions[1])};
-        this.coordinate = coordinate;
-        this.direction = robotInstructions[2].toString() as Direction
+        this.latestLocation ={ coordinate, direction: robotInstructions[2].toString() as Direction};
+        this.trackCommand(Command.PLACE, prevLocation);
     }
     validateLocation = (location:Coordinate) =>{
         return location.x<= this.coordinateLimit.x && location.y <= this.coordinateLimit.y
     }
-    trackCommand = (command: string) =>{
-        this.commands.push({coordiante:this.coordinate, direction: this.direction, command: Command[command as keyof typeof Command]})
+    trackCommand = (command: string,prevLocation: Location) =>{
+        this.commands.push({
+            prevLocation: prevLocation,
+            currentLocation:{...this.latestLocation},
+            command: Command[command as keyof typeof Command]
+        })
+    }
+    printLastNCommands = (count: number) =>{
+        const commandsList =  this.commands.slice(1,count); // need to print the PLACE command
+         commandsList.forEach(({prevLocation,command})=>{
+          console.log("(",prevLocation.coordinate.x, ",", 
+            prevLocation.coordinate.y, ",",
+            prevLocation.direction, ") ",
+            command);
+         });
+        console.log("( ", this.latestLocation.coordinate.x,",", 
+        this.latestLocation.coordinate.y,",", 
+        this.latestLocation.direction,")" );
     }
 }
 
